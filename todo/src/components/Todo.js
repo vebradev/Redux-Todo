7ef5fa1d684cfd4b4336cab4a171dfcc69efd03b
vebradev <justinas@vebra.dev>
2019-06-18 17:36:54 +0300

    Add styles to MVP.

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -2,17 +2,55 @@ import React from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { toggleComplete } from "../App";
+import styled from "styled-components";
+
+const StyledUl = styled.ul`
+  border-radius: 4px;
+  color: #fff;
+  font-size: 20px;
+  margin: 20px auto;
+  display: block;
+  width: fit-content;
+  padding: 0;
+  list-style-type: none;
+
+  li {
+    padding: 15px;
+    margin: 10px 0;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: all 0.3s;
+
+    &:hover {
+        background-color: #1f64b1;
+        color: #fff;
+    }
+  }
+
+  .strike-false {
+    background-color: #5893d4;
+  }
+
+  .strike-true {
+    color: #1ab700;
+      background-color: #b0e0a8;
+  }
+`;
 
 export class Todo extends React.Component {
   render() {
     return (
-      <ul>
+      <StyledUl>
         {this.props.todos.map(item => (
-          <li key={item.id} onClick={() => this.props.toggleComplete(item.id)}>
+          <li
+            key={item.id}
+            onClick={() => this.props.toggleComplete(item.id)}
+            className={`strike-${item.completed}`}
+          >
             {item.value}
           </li>
         ))}
-      </ul>
+      </StyledUl>
     );
   }
 }
--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,27 +1,42 @@
 import React from "react";
 import { connect } from "react-redux";
 import { addTodo } from "../App";
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  background-color: #1f3c88;
+  padding: 20px 40px;
+  border-radius: 4px;
+  border: 1px solid #5893d4;
+  color: #fff;
+  font-size: 20px;
+  margin: 20px auto;
+  display: block;
+`;
 
 class TodoForm extends React.Component {
-    todoInput = React.createRef();
+  todoInput = React.createRef();
 
-    onKeyDown = e => {
-        if (e.key === "Enter") {
-            this.props.addTodo(this.todoInput.current.value);
-            this.todoInput.current.value = "";
-        }
+  onKeyDown = e => {
+    if (e.key === "Enter") {
+      this.props.addTodo(this.todoInput.current.value);
+      this.todoInput.current.value = "";
     }
-    
-    render () {
-        return (
-            <input
-                onKeyDown={this.onKeyDown}
-                ref={this.todoInput}
-                placeholder="Whats next?"
-                type="text"
-            />
-        );
-    }
+  };
+
+  render() {
+    return (
+      <StyledInput
+        onKeyDown={this.onKeyDown}
+        ref={this.todoInput}
+        placeholder="Whats next?"
+        type="text"
+      />
+    );
+  }
 }
 
-export default connect(null, { addTodo })(TodoForm);
+export default connect(
+  null,
+  { addTodo }
+)(TodoForm);
